6b7449ce18ddaf3a6f4fe06e0f4e16db6561183a
edobasky <edore7@gmail.com>
2022-10-15 10:53:30 +0100

    Added delete parent entity and child entity

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -120,5 +120,21 @@ namespace CompanyEmployees.Controllers
                 ids
             }, companyCollectionToReturn);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCompany(Guid id)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with id : {id} doesnt exist in the database. ");
+                return NotFound();
+            }
+
+            _repository.Company.DeleteCompany(company);
+            _repository.Save();
+
+            return NoContent();
+        }
     }
 }
--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -11,5 +11,6 @@ namespace Contracts
         IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
         Company GetCompany(Guid companyId, bool trackChanges);
         void CreateCompany(Company company);
+        void DeleteCompany(Company company);
     }
 }
